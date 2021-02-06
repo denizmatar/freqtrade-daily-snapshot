fd7a35cd7bec0c8de58441b1e6cb87dc598161b4
denizmatar <72666297+denizmatar@users.noreply.github.com>
2021-02-06 12:53:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from analysis import Analysis
 
 
 db = Analysis()
-db.daily_snapshot()
-db.mailer()
+db.daily_snapshot() # --> makes a snapshot from database and writes to a csv file
+db.mailer()         # --> mails the csv file
