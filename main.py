4d9a065135de6f2ef779961be72b829a283d9ac3
denizmatar <denizmatar1@gmail.com>
2021-02-06 13:57:48 +0300

    test

--- main.py
+++ main.py
@@ -4,3 +4,5 @@ from analysis import Analysis
 db = Analysis()
 db.daily_snapshot() # --> makes a snapshot from database and writes to a csv file
 db.mailer()         # --> mails the csv file
+
+# asdasd
